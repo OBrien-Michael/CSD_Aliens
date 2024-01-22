7fc90771eafb0ca7fa6d8baa6c4fc0f29122f7d4
Michael O'Brien <obrien.michael@outlook.ie>
2024-01-22 19:07:28 +0000

    Created skeleton tests for Alien.java and Raygun.java

---
+++ test/com/company/RaygunTest.java
@@ -0,0 +1,24 @@
+package com.company;
+
+import static org.testng.Assert.*;
+
+import org.testng.annotations.Test;
+
+public class RaygunTest {
+
+  @Test
+  public void testFireAt() {
+  }
+
+  @Test
+  public void testRecharge() {
+  }
+
+  @Test
+  public void testIsCharged() {
+  }
+
+  @Test
+  public void testIsFullyCharged() {
+  }
+}
